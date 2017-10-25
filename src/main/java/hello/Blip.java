c827e08b10ef5fa46075efe7283e9abd2f3a7033
deanomo <dean.burson@gmail.com>
2017-10-25 13:59:02 -0700

    first

---
+++ src/main/java/hello/Blip.java
@@ -0,0 +1,19 @@
+package hello;
+
+public class Blip {
+    private long id;
+    private String content;
+
+    public Blip(long id) {
+        this.id = id;
+        content = String.format("this is blip %d", id);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
